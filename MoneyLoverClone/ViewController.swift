4a876ead0c38801542d41fb70e4a65899339dddf
sunset12321215 <sunsetno08@gmail.com>
2020-09-17 17:10:30 +0700

    Initial Commit

---
+++ MoneyLoverClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoneyLoverClone
+//
+//  Created by Vu Xuan Cuong on 9/17/20.
+//  Copyright © 2020 Vu Xuan Cuong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
