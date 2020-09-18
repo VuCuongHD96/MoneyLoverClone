522ab0f9f93afb9159d7064dc9b87708919f96e2
sunset12321215 <sunsetno08@gmail.com>
2020-09-18 17:33:23 +0700

    Create UI

---
+++ MoneyLoverClone/Scenes/Transactions/Cell/TransactionCell.swift
@@ -0,0 +1,23 @@
+//
+//  TransactionCell.swift
+//  MoneyLoverClone
+//
+//  Created by Vu Xuan Cuong on 9/21/20.
+//  Copyright © 2020 Vu Xuan Cuong. All rights reserved.
+//
+
+import UIKit
+import Reusable
+
+final class TransactionCell: UITableViewCell, NibReusable {
+
+    // MARK: - Outlet
+    @IBOutlet private weak var categoryImageView: UIImageView!
+    @IBOutlet private weak var categoryName: UILabel!
+    @IBOutlet private weak var noteLabel: UILabel!
+    @IBOutlet private weak var costLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
