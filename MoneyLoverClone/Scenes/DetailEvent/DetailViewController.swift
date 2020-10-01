d62bf6641b4c3b4911b9bb3787e709d1a36bd1e2
steven <thaikhacmanh1998@gmail.com>
2020-10-01 11:08:35 +0700

    Handel Event

---
+++ MoneyLoverClone/Scenes/DetailEvent/DetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DetailViewController.swift
+//  MoneyLoverClone
+//
+//  Created by V000232 on 9/30/20.
+//  Copyright © 2020 Vu Xuan Cuong. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
