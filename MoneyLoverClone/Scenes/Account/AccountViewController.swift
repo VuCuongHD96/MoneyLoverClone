ac6b1aba86bc36026a17d76adb090c2cde908638
hoang43 <70503946+hoang43@users.noreply.github.com>
2020-10-05 08:46:27 +0700

    fix file changePassWord

--- MoneyLoverClone/Scenes/Account/AccountViewController.swift
+++ MoneyLoverClone/Scenes/Account/AccountViewController.swift
@@ -20,6 +20,7 @@ class AccountViewController: UIViewController {
     var iconArray = ["group", "setting", "changepass", "logout"]
     var myIndex = 0
     var pass = "123"
+    var urlString = "https://lh3.googleusercontent.com/fife/ABSRlIrutP_5kQJAnVeXD4fq3FOnplZj9Avlyxa1uHiZ8QHh8nq5Aj3iZANTl-cMSnTMyLYmVQC8j2l3BNTqD7G9NQPWzj_chlJwerw3k4zDOSNQFow_bFEZ3PF4xnS1UovOzXTNSHajP4BdUICBWxofFtNpIgqSnhWqeFr6HOq18zdvvNO9vYRScnXBQA-K9E6f2sbymlDVebea-x7LX7LXO_jMDA6tbQ9XORGWUrtwLvQmItW9w8N4kf3Hemk-6QLRxF1Gktt__E6YOk236ms_XERbRq-_btHeGIp8lYHHWIIUK_cDFjws7ap63hLgwzOBhK6xNPCgSBCqem1NWn36SfgjOOMWxXZ9sEAU07P9z8SpQLLMrfnGesxFM4OTaCnshrN4U9zJvl3TTAgiVjVhOD-oESDpfSBjTNU-CsBV-Fm01QKeYYzw7LbRfrJAtt8A-r6xkpncpYCjQ3vYcfvsA-12ie5ZDcDDA3_KnHyPcVqDU3sAb_A_BMzkXVyVu8PW84CSA57-RmFow6alL5fmlu0Lymt_KLhvVuUsRRbI4TeAPLmvuIJvjjC8Sp-qUb1jIP5yhjoEBnJdfGuCTsBiMXNPFwC1iVqDxxmn1XDawNvX1MyiMjAH8lry2uH8AUaWR-dQ1eoZrnJW0_gJ_z7BxtMHja7N6p7Qn6SPBD5n3HMOgHy2ONRylM1n8p1JcitOJcXg1MGWA6oQc7tqlK4XpjMWkQZWc2TKLvPBTG1I2Kg46g=s32-c"
     
     struct Constant {
         static let heighForHeader: CGFloat = 45
@@ -34,7 +35,14 @@ class AccountViewController: UIViewController {
     }
     
     func setData() {
-        accountImg.image = UIImage(named: "male")
+        if urlString.isEmpty {
+            accountImg.image = UIImage(named: "male")
+        } else {
+            guard let url = URL(string: urlString) else { return  }
+            accountImg.load(url: url)
+        }
+        accountImg.layer.cornerRadius = accountImg.frame.size.width / 2
+        accountImg.clipsToBounds = true
         nameLabel.text = "Việt Hoàng"
         gmailLable.text = "nguyenviethoang@gmail.com"
         gmailLable.textColor = .lightGray
@@ -133,4 +141,18 @@ extension AccountViewController: UITableViewDelegate, UITableViewDataSource {
 
 extension AccountViewController: StoryboardSceneBased {
     static var sceneStoryboard = Storyboard.account
+}
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
 }
