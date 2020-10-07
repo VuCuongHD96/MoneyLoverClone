77de5acc2bdd5790ab642b54da11fe46cf253fab
cinderela95 <thang.phanminh@V000076s-Mac-mini.local>
2020-10-07 14:40:40 +0700

    login

--- MoneyLoverClone/Application/AppDelegate.swift
+++ MoneyLoverClone/Application/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import RealmSwift
+import Firebase
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         _ = try? Realm()
         print(Realm.Configuration.defaultConfiguration.fileURL!)
+        FirebaseApp.configure()
+        ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:] ) -> Bool { ApplicationDelegate.shared.application(app, open: url, sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String, annotation: options[UIApplication.OpenURLOptionsKey.annotation])
     }
     
     // MARK: UISceneSession Lifecycle
