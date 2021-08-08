da9b25034f0adcd15c2401c2287505a5c8868464
sonnt <>
2021-08-08 22:03:36 +0700

    Task[19.3] Fetch Data

---
+++ MoneyLoverClone/Scenes/ViewControllerType/ViewControllerType.swift
@@ -0,0 +1,14 @@
+//
+//  ViewControllerType.swift
+//  MoneyLoverClone
+//
+//  Created by admin on 8/8/21.
+//  Copyright © 2021 Vu Xuan Cuong. All rights reserved.
+//
+
+import Foundation
+
+protocol ViewControllerType: class {
+    func setupView()
+    func setupData()
+}
